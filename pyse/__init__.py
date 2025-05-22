6ca2ebeb6a27345bde3d2c9464eaaf244ce0b30c
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 <tanaypoddar@hotmail.com>
2025-05-22 11:35:20 -0700

    renamed pyse to core for code hygeine

--- pyse/__init__.py
+++ pyse/__init__.py
@@ -1,1 +1,4 @@
-__all__ = ["pyse", "syllabify", "textgrid", "enrich_features", "a"]
+from .core import SE
+from . import syllabify, textgrid, enrich_features, artics, core
+
+__all__ = ["core", "SE", "syllabify", "textgrid", "enrich_features", "artics"]
